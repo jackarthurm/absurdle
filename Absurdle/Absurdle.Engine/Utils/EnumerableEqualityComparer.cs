cb3ae3e7441d81d8551bb45c51050a65fe2a3453
Jack Mann <jack.mann@hexagon.com>
2022-06-18 18:24:16 +0100

    comments

--- Absurdle/Absurdle.Engine/Utils/EnumerableEqualityComparer.cs
+++ Absurdle/Absurdle.Engine/Utils/EnumerableEqualityComparer.cs
@@ -3,10 +3,16 @@
     /// <summary>
     /// An equality comparer for enumerables.
     /// This makes it possible to hash an enumerable for use as 
-    /// a dictionary key or adding to a hashset.
+    /// a dictionary key or as a hashset member.
     /// </summary>
     internal class EnumerableEqualityComparer<T> : IEqualityComparer<IEnumerable<T>>
     {
+        /// <summary>
+        /// Delegates to the default equality comparer of each item.
+        /// </summary>
+        /// <param name="x">LHS enumerable of <typeparamref name="T"/></param>
+        /// <param name="y">RHS enumerable of <typeparamref name="T"/></param>
+        /// <returns></returns>
         public bool Equals(IEnumerable<T>? x, IEnumerable<T>? y)
         {
             if (x is null || y is null)
@@ -17,7 +23,7 @@
 
         /// <summary>
         /// The hash of the enumerable is computed from 
-        /// the combined hashes of its elements
+        /// the combined hashes of its elements.
         /// </summary>
         /// <param name="enumerable"></param>
         /// <returns></returns>
